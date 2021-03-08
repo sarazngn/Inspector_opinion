df9205fe4c60d02313aa46f396db7aafaff851c9
sarazngn <zangeneh.sara@gmail.com>
2021-03-07 21:22:39 -0800

    Fixed Login View Item Sizes

---
+++ JS/3f837b2f0085b5125112fc455941ea94.js
@@ -0,0 +1,3 @@
+setTimeout(function(){
+  console.log("%cmade with paperclips and rubberbands by https://twitter.com/tonybanik","background:#fff;border:1px solid black;font-family:monospace;padding:0.8em 1.5em;");
+},1000)
